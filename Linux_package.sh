af9e7da09e9cbad51dde77e26806000463e462b6
Nicholas-Stull <nicholasp.stull@gmail.com>
2024-01-27 19:41:19 -0500

    fixed Linux_package.sh

--- Linux_package.sh
+++ Linux_package.sh
@@ -1,10 +1,10 @@
-chmod +x Installer/Linux/uninstall.sh
-chmod +x Installer/Linux/install.sh
-mkdir Installer/Linux/PyPassGen_Linux
-mkdir Installer/Linux/PyPassGen_Linux/src
-cp PyPassGen.py PyPassGen_Linux/
-cp Installer/Linux/install.sh Installer/Linux/PyPassGen_Linux/
-cp Installer/Linux/uninstall.sh Installer/Linux/PyPassGen_Linux/
-cp Installer/Linux/password_generator.desktop Installer/Linux/PyPassGen_Linux/
-cp src/seal.png PyPassGen_Linux/src/
+#chmod +x Installer/Linux/uninstall.sh
+#chmod +x Installer/Linux/install.sh
+#mkdir Installer/Linux/PyPassGen_Linux
+#mkdir Installer/Linux/PyPassGen_Linux/src
+#cp PyPassGen.py PyPassGen_Linux
+#cp Installer/Linux/install.sh Installer/Linux/PyPassGen_Linux
+#cp Installer/Linux/uninstall.sh Installer/Linux/PyPassGen_Linux
+#cp Installer/Linux/password_generator.desktop Installer/Linux/PyPassGen_Linux
+#cp src/seal.png Installer/Linux/PyPassGen_Linux/src
 tar -czvf PyPassGen_Linux.tar.gz PyPassGen_Linux
